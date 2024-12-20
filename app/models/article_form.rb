486d037df02d6b9ab5ad014de29c946da2aec38d
teshio0512 <teshio0512@gmail.com>
2024-12-20 21:34:24 +0900

    画像を添付できるように設定

--- app/models/article_form.rb
+++ app/models/article_form.rb
@@ -1,6 +1,6 @@
 class ArticleForm
   include ActiveModel::Model
-  attr_accessor :theme, :issue, :measure, :result, :user_id, :tag_name
+  attr_accessor :theme, :issue, :measure, :result, :user_id, :tag_name, :image
 
   with_options presence: true do
     validates :theme
@@ -11,7 +11,7 @@ class ArticleForm
   end
 
   def save
-    article = Article.create(theme: theme, issue: issue, measure: measure, result: result, user_id: user_id)
+    article = Article.create(theme: theme, issue: issue, measure: measure, result: result, user_id: user_id, image: image)
     if tag_name.present?
       tag = Tag.where(tag_name: tag_name).first_or_initialize
       tag.save
