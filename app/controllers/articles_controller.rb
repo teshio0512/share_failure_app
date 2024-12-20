d5494313f3268ee9f734a6cd8a4b864692e09061
teshio0512 <teshio0512@gmail.com>
2024-12-20 21:05:51 +0900

    記事投稿の処理をコントローラーに記述

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,24 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order("id ASC")
+  end
+
+  def new
+    @article_form = ArticleForm.new
+  end
+
+  def create
+    @article_form = ArticleForm.new(article_form_params)
+    if @article_form.valid?
+      @article_form.save
+      redirect_to action: :index
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_form_params
+    params.require(:article_form).permit(:theme, :issue, :measure, :result, :tag_name, :image).merge(user_id: current_user.id)
+  end
 end
