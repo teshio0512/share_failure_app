4a3cbf82cd5098ddf9bed314ae14bfab60c78b3a
teshio0512 <teshio0512@gmail.com>
2024-12-21 16:27:22 +0900

    searchアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles, only: [:index, :new, :create]
+  resources :articles, only: [:index, :new, :create] do
+    collection do
+      get 'search'
+    end
+  end
 end
