7ee53c9d03f48a22f130518e866e0ab571f81a3e
teshio0512 <teshio0512@gmail.com>
2024-12-21 16:24:57 +0900

    searchメソッドを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,12 @@ class Article < ApplicationRecord
   has_many :article_tags
   has_many :tags, through: :article_tags
   has_one_attached :image
+
+  def self.search(search)
+    if search != ""
+      Article.where('issue LIKE(?)', "%#{search}%")
+    else
+      Article.all
+    end
+  end
 end
