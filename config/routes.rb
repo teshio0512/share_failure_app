1f66b23b77171fe40059df2a1df1138319ec9288
teshio0512 <teshio0512@gmail.com>
2024-12-22 17:46:42 +0900

    ルーティングにdestroyアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :articles do
     collection do
       get 'search'
     end
