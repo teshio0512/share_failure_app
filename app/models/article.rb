a6eea0eb4c0f0493ce82ecbcba141598ae5fd2c6
teshio0512 <teshio0512@gmail.com>
2024-12-22 17:48:34 +0900

    dependent: :destroyを追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_tags
-  has_many :tags, through: :article_tags
+  has_many :article_tags, dependent: :destroy
+  has_many :tags, through: :article_tags, dependent: :destroy
   has_one_attached :image
 
   def self.search(search)
